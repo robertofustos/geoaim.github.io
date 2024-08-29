89d136c81ab883a61925a84753d8c0dbd4ac4fb6
robertofustos <80533627+robertofustos@users.noreply.github.com>
2024-08-29 11:32:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+    document.getElementById('login-button').addEventListener('click', function() {
+    var Username = document.getElementById('Username').value;
+    var Password = document.getElementById('Password').value;
+  
+    // Aquí puedes realizar la lógica de autenticación, como enviar una solicitud a un servidor o verificar las credenciales localmente.
+    // En este ejemplo, simplemente comprobamos que el usuario sea "admin" y la contraseña sea "password".
+  
+    if (Username === 'Fustos.Roberto' && Password === 'familia4') {
+      window.location.href = 'http://152.74.222.160:1150/';
+    }else if (Username === 'Barrales.Francisca' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1101/'; 
+    }else if (Username === 'Burgos.Juan' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1102/'; 
+    }else if (Username === 'Estuardo.Felipe' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1103/'; 
+    }else if (Username === 'Fernandez.Daniela' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1104/'; 
+    }else if (Username === 'Fernandez.Mathias' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1105/'; 
+    }else if (Username === 'Julio.Marshal' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1106/'; 
+    }else if (Username === 'Lagazzi.Benjamin' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1107/'; 
+    }else if (Username === 'Olea.Pablo' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1108/'; 
+    }else if (Username === 'Ulloa.Kevin' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1109/'; 
+    }else if (Username === 'Aedo.Jorge' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1110/'; 
+    }else if (Username === 'Aravena.Debora' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1111/'; 
+    }else if (Username === 'Arriagada.Sebastian' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1112/'; 
+    }else if (Username === 'Bizama.Martin' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1113/'; 
+    }else if (Username === 'Carrillo.Aladino' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1114/'; 
+    }else if (Username === 'Castillo.Cristobal' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1115/'; 
+    }else if (Username === 'Cuell.Constanza' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1116/'; 
+    }else if (Username === 'Farias.Felipe' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1117/'; 
+    }else if (Username === 'Figueroa.Nelson' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1118/'; 
+    }else if (Username === 'Gomez.Martin' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1119/'; 
+    }else if (Username === 'Gonzalez.Pia' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1120/'; 
+    }else if (Username === 'Gutierrez.Bryan' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1121/'; 
+    }else if (Username === 'Hicks.Bastian' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1122/'; 
+    }else if (Username === 'Jorquera.Carlos' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1123/'; 
+    }else if (Username === 'Liguencura.Javiera' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1124/'; 
+    }else if (Username === 'Medina.Jose' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1125/'; 
+    }else if (Username === 'Montes.Benjamin' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1126/'; 
+    }else if (Username === 'Munoz.Martina' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1127/'; 
+    }else if (Username === 'Ortega.Javier' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1128/'; 
+    }else if (Username === 'Otarola.Shizley' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1129/'; 
+    }else if (Username === 'Parra.Camilo' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1130/'; 
+    }else if (Username === 'Ravena.Marcelo' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1131/'; 
+    }else if (Username === 'Rivas.Javiera' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1132/'; 
+    }else if (Username === 'Roa.Sebastian' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1133/'; 
+    }else if (Username === 'Sazo.Carlos' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1134/'; 
+    }else if (Username === 'Solis.Vicente' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1135/'; 
+    }else if (Username === 'Torres.Javier' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1136/'; 
+    }else if (Username === 'Valdebenito.Saul' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1137/'; 
+    }else if (Username === 'Valdebenito.Fernanda' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1138/'; 
+    }else if (Username === 'Vargas.Francisco' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1139/'; 
+    }else if (Username === 'Yanez.Dariella' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1140/'; 
+    }else if (Username === 'Yanez.Jorge' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1141/'; 
+    }else if (Username === 'Zuniga.Paz' && Password === 'minas1'){
+      window.location.href = 'http://152.74.222.160:1142/'; 
+    } else {
+      alert('Credenciales incorrectas. Por favor, intentalo de nuevo.');
+    }
+    
+  });
